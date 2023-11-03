510410ab73ca6b674b51b881340cf95c175c61df
Vadym Pershoguba <tanakenrip@gmail.com>
2023-11-03 10:57:11 +0100

    3.11 11:00 MC finishied

--- pages/index.js
+++ pages/index.js
@@ -49,3 +49,57 @@ document.getElementById('menuBox').addEventListener('click', ()=>{
   document.getElementById('menuBox').style.display = 'none';
   document.getElementById('creationBox').style.display = 'block';
 });
+
+class Test {
+  constructor() {
+    this.questions = [];
+  }
+
+  addQuestion(question, type, options, answer) {
+    const questionInfo = new Question(question, type, options, answer);
+    this.questions.push(questionInfo);
+  }
+}
+
+const test = new Test(giveId(), 0)
+
+class Question {
+  constructor(question, type, options, answer) {
+    this.question = question;
+    this.type = type;
+    this.options = options;
+    this.answer = answer;
+  }
+}
+
+document.getElementById('addQuestionButton').addEventListener('click', ()=>{
+    let answers = Array.from(document.getElementsByClassName('checkMultipleChoise')).map(input => input.value)
+    test.addQuestion(inputText.value, typeOfQuestion.value, [option1.value, option2.value, option3.value, option4.value], answers)
+    console.log(test)
+});
+
+function giveId () {
+  const min = 1;
+  const max = 1000000000000000000000000000;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+document.getElementById('checkbox1').addEventListener('click', ()=>{
+  document.getElementById('checkbox1').style.backgroundColor = document.getElementById('checkbox1').value == 0 ? 'green' : 'white';
+  document.getElementById('checkbox1').value = document.getElementById('checkbox1').value == 0 ? 1 : 0;
+});
+
+document.getElementById('checkbox2').addEventListener('click', ()=>{
+  document.getElementById('checkbox2').style.backgroundColor = document.getElementById('checkbox2').value == 0 ? 'green' : 'white';
+  document.getElementById('checkbox2').value = document.getElementById('checkbox2').value == 0 ? 1 : 0;
+});
+
+document.getElementById('checkbox3').addEventListener('click', ()=>{
+  document.getElementById('checkbox3').style.backgroundColor = document.getElementById('checkbox3').value == 0 ? 'green' : 'white';
+  document.getElementById('checkbox3').value = document.getElementById('checkbox3').value == 0 ? 1 : 0;
+});
+
+document.getElementById('checkbox4').addEventListener('click', ()=>{
+  document.getElementById('checkbox4').style.backgroundColor = document.getElementById('checkbox4').value == 0 ? 'green' : 'white';
+  document.getElementById('checkbox4').value = document.getElementById('checkbox4').value == 0 ? 1 : 0;
+});
