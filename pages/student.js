0ce44525d606f5e5c27d26c6e41c0f9d0416e2c9
Vadym Pershoguba <tanakenrip@gmail.com>
2023-11-14 21:57:46 +0200

    21:00 14.11

--- pages/student.js
+++ pages/student.js
@@ -12,10 +12,41 @@ document.getElementById('joinSessionButton').addEventListener('click', ()=>{
       .then(data => {
         joinSessionCodeBox.style.display = 'none';
         joinEnterUsername.style.display = 'block';
+        test = data
       })
 })
 
+let test = null;
+
 document.getElementById('joinGetUsernameStart').addEventListener('click', ()=>{
     localStorage.setItem('username', joinGetUsername.value);
-    joinEnterUsername.style.display = 'none'
+    joinEnterUsername.style.display = 'none';
+    drawMultipleChoice()
+    
 })
+
+function drawMultipleChoice(){
+    joinMulptileChoiceDraw.style.display = 'block';
+    joinMCQuestion.textContent = test.test[0].question;
+    
+}
+
+document.getElementById('checkbox1').addEventListener('click', ()=>{
+    document.getElementById('checkbox1').style.backgroundColor = document.getElementById('checkbox1').value == 0 ? 'green' : 'white';
+    document.getElementById('checkbox1').value = document.getElementById('checkbox1').value == 0 ? 1 : 0;
+  });
+  
+  document.getElementById('checkbox2').addEventListener('click', ()=>{
+    document.getElementById('checkbox2').style.backgroundColor = document.getElementById('checkbox2').value == 0 ? 'green' : 'white';
+    document.getElementById('checkbox2').value = document.getElementById('checkbox2').value == 0 ? 1 : 0;
+  });
+  
+  document.getElementById('checkbox3').addEventListener('click', ()=>{
+    document.getElementById('checkbox3').style.backgroundColor = document.getElementById('checkbox3').value == 0 ? 'green' : 'white';
+    document.getElementById('checkbox3').value = document.getElementById('checkbox3').value == 0 ? 1 : 0;
+  });
+  
+  document.getElementById('checkbox4').addEventListener('click', ()=>{
+    document.getElementById('checkbox4').style.backgroundColor = document.getElementById('checkbox4').value == 0 ? 'green' : 'white';
+    document.getElementById('checkbox4').value = document.getElementById('checkbox4').value == 0 ? 1 : 0;
+  });
